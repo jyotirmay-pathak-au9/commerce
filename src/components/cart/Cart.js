11e080c946e82b9ad3363e055c59acb5cde23630
Jyotirmay <jyotirmay362@gmail.com>
2021-04-17 02:07:24 +0530

    incerase quantity added'

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react'
 
 export class Cart extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+            title : 'Loaf',
+            qty: '1',
+            price: '20'
+        }
+        // this.increaseQty = this.increaseQty.bind(this)
+    }
+    
+    increaseQty = () => {
+        console.log("this", this.state)
+    }
+
     render() {
+        const { qty, price} = this.state
+
+       
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img src="" alt="" style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize: 25}}>Loaf</div>
-                    <div style={{color: '#777'}}>Qty: </div>
-                    <div style={{color: '#777'}}> &#x20B9;</div>
-                    <div className="cart-item-actions"></div>
+                    <div style={{fontSize: 25}}>{this.state.title}</div>
+                    <div style={{color: '#777'}}>{qty}</div>
+                    <div style={{color: '#777'}}> &#x20B9; {price}</div>
+                    <div className="cart-item-actions">
+                        <img alt="Incrase" className="action-icons" src="https://as2.ftcdn.net/jpg/01/07/62/07/500_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg" onClick={this.increaseQty} />
+                        <img alt="Decrease" className="action-icons" src="https://as1.ftcdn.net/jpg/03/73/49/86/500_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg" />
+                        <img alt="Delete" className="action-icons" src="https://t4.ftcdn.net/jpg/00/98/26/11/240_F_98261175_Sv69O3rZsHApYkjAdrWbgQixYHwyZyOr.jpg" />
+                    </div>
                 </div>                
             </div>
         )
